6a63f764dcff38dfdef41c8bb07d154e0c9aa480
cskiro <skiroc@ELSPHIM-221373.science.regn.net>
2017-02-19 00:04:06 -0500

    Checking if user input is an integer or not

--- BirthdayWizard/src/birthdaywizard/BirthdayWizard.java
+++ BirthdayWizard/src/birthdaywizard/BirthdayWizard.java
@@ -6,11 +6,13 @@
 package birthdaywizard;
 
 import java.util.Calendar;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
- * Chapter 1 Exercise 9
- * Given the person's year of birth, the program will display how old the user is.
+ * Chapter 1 Exercise 9 Given the person's year of birth, the program will
+ * display how old the user is.
+ *
  * @author skiroc
  */
 public class BirthdayWizard {
@@ -22,19 +24,23 @@ public class BirthdayWizard {
         // Variables
         int yearBorn, age;
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        
+
         Scanner kb = new Scanner(System.in);
         
-        // Prompting user to enter their birth year
-        System.out.println("What year were you born?");
-        yearBorn = kb.nextInt();
-        
-        // Calculating user's age
-        age = currentYear - yearBorn;
-        
-        // Displaying user's age
-        System.out.println("Your are " + age + " years old");
-        
+        try {
+            // Prompting user to enter their birth year
+            System.out.println("What year were you born?");
+            yearBorn = kb.nextInt();
+
+            // Calculating user's age
+            age = currentYear - yearBorn;
+
+            // Displaying user's age
+            System.out.println("Your are " + age + " years old");
+        } catch (InputMismatchException ex) {
+            System.out.println("This is not an integer");
+        } //end try/catch
+
     } // end main
-    
+
 } // end class
